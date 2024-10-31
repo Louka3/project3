3d3f554999ac8bebfaa81a18dd4fb3a303deeafa
Louka3 <lkuczykowski@gmail.com>
2024-10-31 13:26:57 -0400

    Continued adding logic for traversing the list and appending nodes. Need to work on the logic for comparing nodes and re-arranging the list if needed to maintain the order

--- main.js
+++ main.js
@@ -27,6 +27,36 @@ class LinkedList {
       this.tail = node;
     }
   }
+  search(input){
+    // traverse the list to see if the word is already accounted for
+    if(this.head === null){
+      const node = new Node(input);
+      this.head = node;
+      return;
+    }
+    while(current){
+      if(current.data.word === input){
+        current.data.count += 1;
+        // call comparison function
+        // i need a way to change the order of the list after running my comparison
+        break; //! ? i know i need to break out of this loop but i need to do something else before that
+      }
+      else {
+        if(current.next){
+          current = current.next();
+          continue;
+        }
+        else{
+          const node = new Node(input);
+          current.next = node;
+          this.tail = node;
+        }
+      }
+    }
+  }
+  compare(){
+
+  }
 
 }
 
