5dc382e553413e6f6e6a2fe48ca05fc46f628d1a
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-11-01 01:00:07 -0400

    still working on the logic for comparing nodes to change the order of the linked list to decreasing order

--- main.js
+++ main.js
@@ -8,21 +8,25 @@ const Node = (input) => {
     word: input,
     count: 1
   }
+  this.prev = null;
 }
 
 class LinkedList {
   constructor(){
     // start the list
     this.head = null;
-    this.tail = null;
+    // this.tail = null;
   }
-  append(word){
+  append(input){
     // append the node to the end of the list
-    const node = new Node(word);
+    const node = new Node(input);
     if(this.head === null){
       this.head = node;
+      this.tail = node; 
     }
     else {
+      // put the node at the end of the list
+      node.prev = this.tail;
       this.tail.next = node;
       this.tail = node;
     }
@@ -30,15 +34,31 @@ class LinkedList {
   search(input){
     // traverse the list to see if the word is already accounted for
     if(this.head === null){
-      const node = new Node(input);
-      this.head = node;
-      return;
+      this.append(input);
     }
+    let current = this.head;
     while(current){
       if(current.data.word === input){
         current.data.count += 1;
-        // call comparison function
-        // i need a way to change the order of the list after running my comparison
+        // I want the nodes with the highest count to be at the front of the list, since the list should be
+        // in decreasing order. 
+        // check the current count vs the count of the previous node
+        if(current.prev === null) return
+        if(current.data.count > current.prev.data.count){
+          //* this need to be looped. but whats the condition?
+          const tempNext = current.next; // ref to next node
+          //! what if prev is null? or if the prev of the prev is null?
+          const newPrev = current.prev.prev;
+          const swapping = current.prev;
+          current.next = swapping;
+          current.prev = newPrev;
+          swapping.next = tempNext;
+          swapping.perv = current;
+        }
+         
+
+          // if the count of current is lower or the same, i can return out of search
+        
         break; //! ? i know i need to break out of this loop but i need to do something else before that
       }
       else {
@@ -47,9 +67,8 @@ class LinkedList {
           continue;
         }
         else{
-          const node = new Node(input);
-          current.next = node;
-          this.tail = node;
+          this.append(input);
+          current = false;
         }
       }
     }
